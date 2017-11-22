37a3f1b3528c96d389bd3dc43490425090dd58e7
Víctor Valle <victormvs022@gmail.com>
2017-11-22 16:56:24 +0100

    Añadidos datos del formulario de creación de un artículo. Falta que funcione el two-way-data-binding

--- src/app/components/article.component.ts
+++ src/app/components/article.component.ts
@@ -1,19 +1,28 @@
 import {Component, OnInit} from '@angular/core';
-
+import {ActivatedRoute, Router} from "@angular/router";
+import {ArticlesService} from "../services/articles.service";
+import {Article} from "../models/article";
 
 @Component({
   selector: 'article-add',
-  templateUrl: '../views/article-add.html'
+  templateUrl: '../views/article-add.html',
+  providers: [ArticlesService]
 })
 export class ArticleComponent implements OnInit {
   public title: string;
-  public subTitle: string;
+  public article: Article;
 
   constructor() {
     this.title = 'Create new Article';
+    this.article = new Article(null, '', '', null, '', null);
   }
 
   ngOnInit() {
     console.log('Se ha cargado el componente ARTICLE');
   }
+
+  onSubmit() {
+    console.log(this.article);
+  }
+
 }
